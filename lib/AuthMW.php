ecb712ee31aac207d05c2f2c1410e61a6b6e84b0
Sebastian Mancke <s.mancke@tarent.de>
2013-10-30 15:58:04 +0100

    added auth middleware

---
+++ lib/AuthMW.php
@@ -0,0 +1,24 @@
+<?php
+
+class AuthMW extends \Slim\Middleware
+{
+    protected $cfg;
+
+    function __construct($cfg) {
+        $this->cfg = $cfg;
+    }
+    
+    public function call()
+    {
+        $cookieValueJson = $this->app->getCookie('backle_auth');
+        if ($cookieValueJson) {
+            $cookieValue = json_decode($cookieValueJson);
+            $this->app->user = $cookieValue;
+        } else {
+            $this->app->user = null;
+        }
+
+        // Run inner middleware and application
+        $this->next->call();
+    }
+}
